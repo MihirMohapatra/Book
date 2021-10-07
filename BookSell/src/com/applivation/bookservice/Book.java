bcf9265dd697099a641cc039d7766cd865451c0d
mihir <mihirmohapatra77@gmail.com>
2021-10-08 00:53:48 +0530

    second commit

--- BookSell/src/com/applivation/bookservice/Book.java
+++ BookSell/src/com/applivation/bookservice/Book.java
@@ -3,4 +3,24 @@ package com.applivation.bookservice;
 public class Book {
 	private String name;
 	private String author;
+	public Book() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", author=" + author + "]";
+	}
+	
 }
---
+++ BookSell/src/com/applivation/bookservice/Test.java
@@ -0,0 +1,12 @@
+package com.applivation.bookservice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Book book = new Book();
+		book.setName("Hope");
+		book.setAuthor("Mihir");
+		System.out.println(book);
+	}
+
+}
